887b98a3c82f4e73ba8baa833d5840a6718e3a1f
Ivan Silva <silvaivan280@gmail.com>
2023-05-28 11:37:24 -0300

    agregue un state al componente Titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function Titulo(props){
+
+    const [msj, setMsj] = useState('')
+
     return (
         <div>
-            <h1>Hello {props.nuevoTitulo}</h1>
+            <h1>Hello {props.nuevoTitulo} {msj}</h1>
+            <button onClick={()=> setMsj('from changed state')}>Click me</button>
         </div>
     );
 }
